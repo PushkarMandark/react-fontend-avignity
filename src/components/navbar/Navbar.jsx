65a642873d0a9e935e411588709df60d156c994e
PushkarMandark <pushkar.meclpu@gmail.com>
2022-02-10 14:00:20 +0530

    banner work

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -3,6 +3,28 @@ import { RiMenu3Line, RiCloseLine } from "react-icons/ri";
 import "./navbar.css";
 import logo from "../../assets/logo.svg";
 
+const Menu = () => {
+  return (
+    <>
+      <p>
+        <a href="#home">Home</a>
+      </p>
+      <p>
+        <a href="#avig">What is Avignity</a>
+      </p>
+      <p>
+        <a href="#possibility">Open AI</a>
+      </p>
+      <p>
+        <a href="#features">Case Studies</a>
+      </p>
+      <p>
+        <a href="#blog">Library</a>
+      </p>
+    </>
+  );
+};
+
 const Navbar = () => {
   const [toogleMenu, setToogleMenu] = useState(false);
   return (
@@ -12,28 +34,14 @@ const Navbar = () => {
           <img src={logo} alt="logo" />
         </div>
         <div className="gpt3__navbar-links_container">
-          <p>
-            <a href="#home">Home</a>
-          </p>
-          <p>
-            <a href="#avig">What is Avignity</a>
-          </p>
-          <p>
-            <a href="#possibility">Open AI</a>
-          </p>
-          <p>
-            <a href="#features">Case Studies</a>
-          </p>
-          <p>
-            <a href="#blog">Library</a>
-          </p>
+          <Menu />
         </div>
       </div>
       <div className="gpt3__navbar-sign">
         <p>Sign in</p>
         <button type="button">Sign Up</button>
       </div>
-      <div className="gtp3__navbar-menu">
+      <div className="gpt3__navbar-menu">
         {toogleMenu ? (
           <RiCloseLine
             color="#fff"
@@ -46,6 +54,17 @@ const Navbar = () => {
             size={27}
             onClick={() => setToogleMenu(true)}
           />
+        )}
+        {toogleMenu && (
+          <div className="gpt3__navbar-menu_container scale-up-center">
+            <div className="gpt3__navbar-menu_container-links">
+              <Menu />
+              <div className="gpt3__navbar-menu_container-links-sign">
+                <p>Sign in</p>
+                <button type="button">Sign Up</button>
+              </div>
+            </div>
+          </div>
         )}
       </div>
     </div>
--- src/container/header/Header.jsx
+++ src/container/header/Header.jsx
@@ -1,8 +1,34 @@
 import React from "react";
 import "./header.css";
-
+import people from "../../assets/people.png";
+import ai from "../../assets/ai.png";
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div className="gpt3__header section__padding" id="home">
+      <div className="gpt3__header-content">
+        <h1 className="gradient__text">
+          Let's Build Something amazing with GPT-3 OpenAI
+        </h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam illum
+          cumque explicabo dicta unde aspernatur necessitatibus esse commodi
+          omnis ratione. Cupiditate amet molestiae nihil, hic dolore aperiam
+          dicta omnis. Aliquam?
+        </p>
+        <div className="gpt3__header-content__input">
+          <input type="email" name="" id="" placeholder="Your Email Address" />
+          <button type="button">Get Started</button>
+        </div>
+        <div className="gpt3__header__people">
+          <img src={people} alt="people" />
+          <p>1,600 People requested access a visit in last 24 hours</p>
+        </div>
+        <div className="gpt3__header-image">
+          <img src={ai} alt="ai" />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
